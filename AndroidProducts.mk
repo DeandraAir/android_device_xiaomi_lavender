f16d5d0d2423c4881581753aabf12e42c9d85546
DeandraAir <affaantuh407@gmail.com>
2023-11-16 06:09:16 +0700

    dean

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/serasa_lavender.mk
+
+COMMON_LUNCH_CHOICES := \
+    serasa_lavender-user \
+    serasa_lavender-userdebug \
+    serasa_lavender-eng
