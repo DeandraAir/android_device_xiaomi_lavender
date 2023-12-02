803a4d876be460970a0f1b168a066295d07025e6
Deandra Aditya <142644567+DeandraAir@users.noreply.github.com>
2023-12-03 00:08:10 +0700

    Update and rename serasa_lavender.mk to droidx_lavender.mk

--- droidx_lavender.mk
+++ droidx_lavender.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common SerasaOS stuff
-$(call inherit-product, vendor/serasa/config/common_full_phone.mk)
+# Inherit some common DroidX-UI stuff
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -19,13 +19,14 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
-WITH_GAPPS := true
+TARGET_USE_MOTO_CALCULATOR := true
+DROIDX_GAPPS := true
 
 # Device Identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lavender
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := serasa_lavender
+PRODUCT_NAME := droidx_lavender
 PRODUCT_MODEL := Redmi Note 7
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
