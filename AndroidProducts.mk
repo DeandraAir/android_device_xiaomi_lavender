0ef72b7b8955c7db1f58e12effb747aaa84eb415
Deandra Aditya <142644567+DeandraAir@users.noreply.github.com>
2023-12-12 20:38:26 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/droidx_lavender.mk
+    $(LOCAL_DIR)/aosp_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    droidx_lavender-user \
-    droidx_lavender-userdebug \
-    droidx_lavender-eng
+    aosp_lavender-user \
+    aosp_lavender-userdebug \
+    aosp_lavender-eng
